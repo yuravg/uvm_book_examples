147c47f6c655cb77872fc32f37e4d4d48f472e7b
Yuriy VG <yuravg@gmail.com>
2023-08-24 15:51:26 +0300

    ex4-21b_callbacks: refactor indentation.

--- uvm_book/examples_lib/04_uvm_library_basics/ex4-21b_callbacks.sv
+++ uvm_book/examples_lib/04_uvm_library_basics/ex4-21b_callbacks.sv
@@ -1,74 +1,77 @@
 /**********************************************************************
-  Example 4-21b: Callbacks
+ Example 4-21b: Callbacks
 
-  To run:   %  irun -uvm ex4-21b_callbacks.sv
-**********************************************************************/
+ To run:   %  irun -uvm ex4-21b_callbacks.sv
+ **********************************************************************/
 
 module top;
-import uvm_pkg::*;
+  import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-virtual class inj_err_cb extends uvm_callback;
-  function new(string name = "inj_err_cb");
-    super.new(name);
-  endfunction : new
-  pure virtual function void inject_err(uvm_object pkt);
-endclass
+  virtual class inj_err_cb extends uvm_callback;
+    function new(string name = "inj_err_cb");
+      super.new(name);
+    endfunction : new
 
-class driver extends uvm_component;
-  `uvm_register_cb(driver, inj_err_cb)
-  `uvm_component_utils(driver)
+    pure virtual function void inject_err(uvm_object pkt);
+  endclass : inj_err_cb
 
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
+  class driver extends uvm_component;
+    `uvm_register_cb(driver, inj_err_cb)
+    `uvm_component_utils(driver)
 
-  virtual task drive_transfer(uvm_object pkt);
-   `uvm_do_callbacks(driver, inj_err_cb, inject_err(pkt))
-   // drive DUT signals
-  endtask : drive_transfer
+    function new(string name, uvm_component parent);
+      super.new(name, parent);
+    endfunction : new
 
-endclass : driver
+    virtual task drive_transfer(uvm_object pkt);
+      `uvm_do_callbacks(driver, inj_err_cb, inject_err(pkt))
+      // drive DUT signals
+    endtask : drive_transfer
+  endclass : driver
 
-typedef uvm_callbacks#(driver, inj_err_cb) inj_err_cb_type;
+  typedef uvm_callbacks #(driver, inj_err_cb) inj_err_cb_type;
 
-class my_driver_cb extends inj_err_cb;
-  function new(string name="my_driver_cb"); super.new(name); endfunction
-  virtual function void inject_err(uvm_object pkt);
-    `uvm_info("my_driver_cb", "injecting an error into the packet", UVM_LOW)
-  endfunction
-endclass
+  class my_driver_cb extends inj_err_cb;
+    function new(string name="my_driver_cb");
+      super.new(name);
+    endfunction : new
 
-class agent extends uvm_component; 
-  `uvm_component_utils(agent)
-  driver my_driver;
+    virtual function void inject_err(uvm_object pkt);
+      `uvm_info("my_driver_cb", "injecting an error into the packet", UVM_LOW)
+    endfunction : inject_err
+  endclass : my_driver_cb
 
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-    my_driver = driver::type_id::create("my_driver", this);
-  endfunction : new
+  class agent extends uvm_component;
+    `uvm_component_utils(agent)
 
-endclass : agent
+    driver my_driver;
 
-class my_project_driver extends driver;
-  `uvm_component_utils(my_project_driver)
+    function new(string name, uvm_component parent);
+      super.new(name, parent);
+      my_driver = driver::type_id::create("my_driver", this);
+    endfunction : new
+  endclass : agent
 
-  virtual task drive_transfer(uvm_object pkt);
-    super.drive_transfer(pkt);
-    `uvm_info("MYINFO1", "Finished driving transfer", UVM_LOW)
-  endtask
-  function new (string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
-endclass : my_project_driver
+  class my_project_driver extends driver;
+    `uvm_component_utils(my_project_driver)
 
-my_driver_cb cb = new();
-agent my_agent;
+    virtual task drive_transfer(uvm_object pkt);
+      super.drive_transfer(pkt);
+      `uvm_info("MYINFO1", "Finished driving transfer", UVM_LOW)
+    endtask : drive_transfer
 
-initial begin
-my_agent = new("my_agent",null);
-inj_err_cb_type::add(null, cb);
-run_test();
-end
+    function new (string name, uvm_component parent);
+      super.new(name, parent);
+    endfunction : new
+  endclass : my_project_driver
 
+  my_driver_cb cb = new();
+  agent my_agent;
+
+  initial begin
+    my_agent = new("my_agent",null);
+    inj_err_cb_type::add(null, cb);
+    run_test();
+  end
 endmodule : top
