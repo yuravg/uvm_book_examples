36f8af37049f909f2abecd650729ec969fdf158d
Yuriy VG <yuravg@gmail.com>
2023-08-24 15:43:43 +0300

    uvm_book: refactor indentation.

--- uvm_book/examples_lib/04_uvm_library_basics/ex4-15_analysis_imp.sv
+++ uvm_book/examples_lib/04_uvm_library_basics/ex4-15_analysis_imp.sv
@@ -1,10 +1,10 @@
 /****************************************************************
-  Example 4-15: Component with an Analysis Import
+ Example 4-15: Component with an Analysis Import
 
-  To run:   %  irun -uvm ex4-15_analysis_imp.sv
+ To run:   %  irun -uvm ex4-15_analysis_imp.sv
 
-  Or:       %  irun -uvmhome $UVM_HOME ex4-15_analysis_imp.sv
-****************************************************************/
+ Or:       %  irun -uvmhome $UVM_HOME ex4-15_analysis_imp.sv
+ ****************************************************************/
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 `include "simple_packet.sv"
@@ -16,8 +16,8 @@ class packet_checker extends uvm_component;
   function new(string name, uvm_component parent);
     super.new(name, parent);
     analysis_export = new("analysis_export", this);
-  endfunction
-  
+  endfunction : new
+
   `uvm_component_utils(packet_checker)
 
   function void write(simple_packet packet);
