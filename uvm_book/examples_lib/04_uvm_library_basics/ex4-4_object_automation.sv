9851a1fd423cac9931eb7b34aea7a20c95d2fbb0
Yuriy VG <yuravg@gmail.com>
2023-08-24 14:51:39 +0300

    uvm_book: refactor indentation, add stdout as comment.

--- uvm_book/examples_lib/04_uvm_library_basics/ex4-4_object_automation.sv
+++ uvm_book/examples_lib/04_uvm_library_basics/ex4-4_object_automation.sv
@@ -1,19 +1,19 @@
 /**********************************************************************
-  Example 4-4: UVM Object Automation
+ Example 4-4: UVM Object Automation
 
-  To run:   %  irun -uvm ex4-4_object_automation.sv
-  OR:       %  irun -uvmhome $UVM_HOME ex4-4_object_automation.sv
-**********************************************************************/
+ To run:   %  irun -uvm ex4-4_object_automation.sv
+ OR:       %  irun -uvmhome $UVM_HOME ex4-4_object_automation.sv
+ **********************************************************************/
 package apb_pkg;
   import uvm_pkg::*;
-  `include "uvm_macros.svh"
-  `include "apb_transfer.sv"
+`include "uvm_macros.svh"
+`include "apb_transfer.sv"
   // Other files for APB UVC go here
 endpackage : apb_pkg
 
 module automation_example;
   import uvm_pkg::*;
-  `include "uvm_macros.svh"
+`include "uvm_macros.svh"
   import apb_pkg::*;
 
   apb_transfer my_xfer, tx1, tx2, tx3;
@@ -22,16 +22,33 @@ module automation_example;
     my_xfer = apb_transfer::type_id::create("my_xfer");
     if (!my_xfer.randomize())
       `uvm_fatal("RANDFAIL", "can not randomize my_xfer")
-    tx1 = my_xfer;              // tx1 and my_xfer share the same memory
+    tx1 = my_xfer;                           // tx1 and my_xfer share the same memory
     tx2 = apb_transfer::type_id::create("tx2");
-    tx2.copy(tx1);              // copies fields from tx1 to tx2
-    $cast(tx3, tx1.clone());    // creates a new apb_transfer and copy all
-                                // fields from tx1 to tx3
+    tx2.copy(tx1);                           // copies fields from tx1 to tx2
+    $cast(tx3, tx1.clone());                 // creates a new apb_transfer and copy all
+                                             // fields from tx1 to tx3
     if (!tx3.compare(tx2))
       `uvm_error("CompareFailed", "The comparison failed")
-    my_xfer.print();            // Prints my_xfer in a table format
-    my_xfer.print(uvm_default_tree_printer);  // Prints in "tree" format
-    my_xfer.print(uvm_default_line_printer);  // Prints in "line" format
+    my_xfer.print();                         // Prints my_xfer in a table format
+    my_xfer.print(uvm_default_tree_printer); // Prints in "tree" format
+    my_xfer.print(uvm_default_line_printer); // Prints in "line" format
   end
-
 endmodule : automation_example
+
+// OUTPUT:
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # my_xfer           apb_transfer        -     @355
+// #   addr            integral            32    'h6288fc4c
+// #   data            integral            32    'ha7764686
+// #   direction       apb_direction_enum  1     APB_WRITE
+// #   transmit_delay  integral            32    'h5
+// # ------------------------------------------------------
+// # my_xfer: (apb_transfer@355) {
+// #   addr: 'h6288fc4c
+// #   data: 'ha7764686
+// #   direction: APB_WRITE
+// #   transmit_delay: 'h5
+// # }
+// # my_xfer: (apb_transfer@355) { addr: 'h6288fc4c  data: 'ha7764686  direction: APB_WRITE  transmit_delay: 'h5  }
--- uvm_book/examples_lib/04_uvm_library_basics/ex4-6_default_config.sv
+++ uvm_book/examples_lib/04_uvm_library_basics/ex4-6_default_config.sv
@@ -1,80 +1,117 @@
 /**********************************************************************
-  Example 4-6: Configuration Mechanism (no configuration)
+ Example 4-6: Configuration Mechanism (no configuration)
 
-  To run:   %  irun -uvm ex4-6_default_config.sv
-  OR:       %  irun -uvmhome $UVM_HOME ex4-6_default_config.sv
-**********************************************************************/
+ To run:   %  irun -uvm ex4-6_default_config.sv
+ OR:       %  irun -uvmhome $UVM_HOME ex4-6_default_config.sv
+ **********************************************************************/
 package my_pkg;
-import uvm_pkg::*;
+  import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-class master_comp extends uvm_component;
-  `uvm_component_utils(master_comp)
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
-  task run_phase (uvm_phase phase);
-    `uvm_info("MASTER", "run_phase: Executing.", UVM_LOW)
-  endtask : run_phase
-endclass : master_comp
+  class master_comp extends uvm_component;
+    `uvm_component_utils(master_comp)
 
-class slave_comp extends uvm_component;
-  `uvm_component_utils(slave_comp)
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
-  task run_phase (uvm_phase phase);
-    `uvm_info("SLAVE", "run_phase: Executing.", UVM_LOW)
-  endtask : run_phase
-endclass : slave_comp
+    function new(string name, uvm_component parent);
+      super.new(name, parent);
+    endfunction : new
 
-class interface_comp extends uvm_component;
-  master_comp master[];  // dynamic array of masters
-  slave_comp  slave[];   // dynamic array of slaves
-  int num_masters = 1;
-  int num_slaves = 1;
-  string if_name = "bus_uvc";
+    task run_phase (uvm_phase phase);
+      `uvm_info("MASTER", "run_phase: Executing.", UVM_LOW)
+    endtask : run_phase
+  endclass : master_comp
 
-  `uvm_component_utils_begin(interface_comp)
-     `uvm_field_string(if_name, UVM_DEFAULT)
-     `uvm_field_int(num_masters, UVM_DEFAULT)
-     `uvm_field_int(num_slaves, UVM_DEFAULT)
-  `uvm_component_utils_end
+  class slave_comp extends uvm_component;
+    `uvm_component_utils(slave_comp)
 
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
+    function new(string name, uvm_component parent);
+      super.new(name, parent);
+    endfunction : new
 
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    master = new[num_masters];
-    slave = new[num_slaves];
-    foreach (master[i])
-      master[i] = master_comp::type_id::create($sformatf("master[%0d]",i), this);
-    foreach (slave[i])
-      slave[i]  = slave_comp::type_id::create($sformatf("slave[%0d]",i), this);
-  endfunction : build_phase
-  task run_phase (uvm_phase phase);
-   `uvm_info("UVC", {"run_phase: Hierarchy:\n",this.sprint()}, UVM_LOW)
-   `uvm_info("UVC", $sformatf("%s has %0d master(s) and %0d slave(s)",
-               if_name, num_masters, num_slaves), UVM_LOW)
-  endtask : run_phase
-endclass : interface_comp
+    task run_phase (uvm_phase phase);
+      `uvm_info("SLAVE", "run_phase: Executing.", UVM_LOW)
+    endtask : run_phase
+  endclass : slave_comp
+
+  class interface_comp extends uvm_component;
+    master_comp master[];  // dynamic array of masters
+    slave_comp  slave[];   // dynamic array of slaves
+    int num_masters = 1;
+    int num_slaves = 1;
+    string if_name = "bus_uvc";
+
+    `uvm_component_utils_begin(interface_comp)
+      `uvm_field_string(if_name, UVM_DEFAULT)
+      `uvm_field_int(num_masters, UVM_DEFAULT)
+      `uvm_field_int(num_slaves, UVM_DEFAULT)
+    `uvm_component_utils_end
+
+    function new(string name, uvm_component parent);
+      super.new(name, parent);
+    endfunction : new
+
+    function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      master = new[num_masters];
+      slave = new[num_slaves];
+      foreach (master[i])
+        master[i] = master_comp::type_id::create($sformatf("master[%0d]",i), this);
+      foreach (slave[i])
+        slave[i]  = slave_comp::type_id::create($sformatf("slave[%0d]",i), this);
+    endfunction : build_phase
+
+    task run_phase (uvm_phase phase);
+      `uvm_info("UVC", {"run_phase: Hierarchy:\n",this.sprint()}, UVM_LOW)
+      `uvm_info("UVC", $sformatf("%s has %0d master(s) and %0d slave(s)",
+                                 if_name, num_masters, num_slaves), UVM_LOW)
+    endtask : run_phase
+  endclass : interface_comp
 endpackage : my_pkg
 
 module test;
-import uvm_pkg::*;
+  import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-import my_pkg::*;
+  import my_pkg::*;
 
-interface_comp my_uvc;
+  interface_comp my_uvc;
 
-initial begin
-// Create components
-  my_uvc = interface_comp::type_id::create("my_uvc", null);
-// Start UVM Phases
-  run_test();
-end
-
+  initial begin
+    // Create components
+    my_uvc = interface_comp::type_id::create("my_uvc", null);
+    // Start UVM Phases
+    run_test();
+  end
 endmodule : test
+
+// OUTPUT:
+// # UVM_INFO @ 0: reporter [RNTST] Running test ...
+// # UVM_INFO ex4-6_default_config.sv(63) @ 0: my_uvc [UVC] run_phase: Hierarchy:
+// # --------------------------------------------
+// # Name           Type            Size  Value
+// # --------------------------------------------
+// # my_uvc         interface_comp  -     @355
+// #   master[0]    master_comp     -     @369
+// #   slave[0]     slave_comp      -     @377
+// #   if_name      string          7     bus_uvc
+// #   num_masters  integral        32    'h1
+// #   num_slaves   integral        32    'h1
+// # --------------------------------------------
+// #
+// # UVM_INFO ex4-6_default_config.sv(64) @ 0: my_uvc [UVC] bus_uvc has 1 master(s) and 1 slave(s)
+// # UVM_INFO ex4-6_default_config.sv(31) @ 0: my_uvc.slave[0] [SLAVE] run_phase: Executing.
+// # UVM_INFO ex4-6_default_config.sv(19) @ 0: my_uvc.master[0] [MASTER] run_phase: Executing.
+// # UVM_INFO verilog_src/uvm-1.2/src/base/uvm_report_server.svh(847) @ 0: reporter [UVM/REPORT/SERVER]
+// # --- UVM Report Summary ---
+// #
+// # ** Report counts by severity
+// # UVM_INFO :    8
+// # UVM_WARNING :    0
+// # UVM_ERROR :    0
+// # UVM_FATAL :    0
+// # ** Report counts by id
+// # [MASTER]     1
+// # [Questa UVM]     2
+// # [RNTST]     1
+// # [SLAVE]     1
+// # [UVC]     2
+// # [UVM/RELNOTES]     1
