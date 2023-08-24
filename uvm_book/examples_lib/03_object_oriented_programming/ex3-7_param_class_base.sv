7a30c55f071bff3a3fd94525a1d74b637bf4c66b
Yuriy VG <yuravg@gmail.com>
2023-08-24 14:39:19 +0300

    ex3-7_param_class_base.sv: extend stdout.

--- uvm_book/examples_lib/03_object_oriented_programming/ex3-7_param_class_base.sv
+++ uvm_book/examples_lib/03_object_oriented_programming/ex3-7_param_class_base.sv
@@ -39,11 +39,15 @@ module top;
   stack #(real) real_stack;    // stack of reals
 
   int int_value;
+  int int_value2;
   bit[9:0] bit_value;
   real real_value;
 
   initial begin
-    int_stack=new(); bit_stack=new(); real_stack=new();
+    int_stack=new();  // id = 1
+    bit_stack=new();  // id = 2
+    real_stack=new(); // id = 3
+
     int_stack.push(400);
     bit_stack.push('h200);
     real_stack.push(40.5);
@@ -51,15 +55,17 @@ module top;
     bit_stack.pop(bit_value);
     real_stack.pop(real_value);
 
-    $display("int:%0d bit:%0h real:%g", int_value, bit_value, real_value);
-
-    int_stack2=new();
+    int_stack2=new();  // id = 4
     int_stack2.push(888);
-    int_stack2.pop(int_value);
+    int_stack2.pop(int_value2);
 
-    $display("int_stack.id=%0d  int_stack2.id=%0d  bit_stack.id=%0d  real_stack.id=%0d",
+    $display("int:%0d; int2:%0d; bit[9:0]:0x%0h; real:%g",
+             int_value, int_value2, bit_value, real_value);
+
+    $display("int_stack.id=%0d; int_stack2.id=%0d; bit_stack.id=%0d; real_stack.id=%0d",
              int_stack.get_id(), int_stack2.get_id(), bit_stack.get_id(), real_stack.get_id());
   end
+
 endmodule : top
 
 // OUTPUT:
@@ -69,7 +75,7 @@ endmodule : top
 // # executed pop(int)
 // # executed pop(bit[9:0])
 // # executed pop(real)
-// # int:400 bit:200 real:40.5
 // # executed push(int)
 // # executed pop(int)
-// # int_stack.id=1  int_stack2.id=4  bit_stack.id=2  real_stack.id=3
+// # int:400; int2:888; bit[9:0]:0x200; real:40.5
+// # int_stack.id=1; int_stack2.id=4; bit_stack.id=2; real_stack.id=3
