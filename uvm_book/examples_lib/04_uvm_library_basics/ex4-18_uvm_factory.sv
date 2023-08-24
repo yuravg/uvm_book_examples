58a0915a335cb64b1b2408f8f748767371f9c50c
Yuriy VG <yuravg@gmail.com>
2023-08-24 15:47:09 +0300

    ex4-18_uvm_factory: fix factory.set_type_override_by_type()
    This change fix QuestaSim error:
    ** Error (suppressible): ex4-18_uvm_factory.sv(66): (vopt-7063) Failed
             to find 'factory' in hierarchical name
             'factory.set_type_override_by_type'.

--- uvm_book/examples_lib/04_uvm_library_basics/ex4-18_uvm_factory.sv
+++ uvm_book/examples_lib/04_uvm_library_basics/ex4-18_uvm_factory.sv
@@ -31,16 +31,6 @@ package my_pkg;
     endtask : drive_transfer
   endclass : driver
 
-  class agent extends uvm_component;
-    `uvm_component_utils(agent)
-    driver my_driver;
-
-    function new(string name, uvm_component parent);
-      super.new(name, parent);
-      my_driver = driver::type_id::create("my_driver", this);
-    endfunction : new
-  endclass : agent
-
   class my_project_driver extends driver;
     `uvm_component_utils(my_project_driver)
 
@@ -52,6 +42,16 @@ package my_pkg;
       super.new(name, parent);
     endfunction : new
   endclass : my_project_driver
+
+  class agent extends uvm_component;
+    `uvm_component_utils(agent)
+    driver my_driver;
+
+    function new(string name, uvm_component parent);
+      super.new(name, parent);
+      my_driver = driver::type_id::create("my_driver", this);
+    endfunction : new
+  endclass : agent
 endpackage : my_pkg
 
 module test;
@@ -62,11 +62,33 @@ module test;
   agent my_agent;
 
   initial begin
+    // Configure BEFORE create
     // factory is a field in the UVM package (uvm_pkg)
+    uvm_factory factory = uvm_coreservice_t::get().get_factory();
     factory.set_type_override_by_type(driver::get_type(), my_project_driver::get_type());
+
     // Create the agent
     my_agent = agent::type_id::create("my_agent", null);
+
     // Run the uvm_phases
     run_test();
   end
 endmodule : test
+
+// OUTPUT:
+// # UVM_INFO @ 0: reporter [RNTST] Running test ...
+// # UVM_INFO ex4-18_uvm_factory.sv(29) @ 0: my_agent.my_driver [MYINFO] Begin Driving transfer
+// # UVM_INFO ex4-18_uvm_factory.sv(39) @ 0: my_agent.my_driver [MYINFO] Finished Driving Transfer
+// # UVM_INFO verilog_src/uvm-1.2/src/base/uvm_report_server.svh(847) @ 0: reporter [UVM/REPORT/SERVER]
+// # --- UVM Report Summary ---
+// #
+// # ** Report counts by severity
+// # UVM_INFO :    6
+// # UVM_WARNING :    0
+// # UVM_ERROR :    0
+// # UVM_FATAL :    0
+// # ** Report counts by id
+// # [MYINFO]     2
+// # [Questa UVM]     2
+// # [RNTST]     1
+// # [UVM/RELNOTES]     1
