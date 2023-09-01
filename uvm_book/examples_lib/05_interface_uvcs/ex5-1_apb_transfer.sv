b740a0b26cadef0029883b4a133a56eda126ace1
Yuriy VG <yuravg@gmail.com>
2023-09-01 15:01:33 +0300

    ex5: refactor indentation, add stdout as comment.

--- uvm_book/examples_lib/05_interface_uvcs/ex5-1_apb_transfer.sv
+++ uvm_book/examples_lib/05_interface_uvcs/ex5-1_apb_transfer.sv
@@ -1,60 +1,97 @@
 /****************************************************************
-  Example 5-1: APB Transfer
+ Example 5-1: APB Transfer
 
-  The APB Transfer class definition plus a simple test to create,
-  randomize and print three transfers
+ The APB Transfer class definition plus a simple test to create,
+ randomize and print three transfers
 
-  To run:   %  irun -uvm ex5-1_apb_transfer.sv
+ To run:   %  irun -uvm ex5-1_apb_transfer.sv
 
-  OR:       %  irun -uvmhome $UVM_HOME ex5-1_apb_transfer.sv
+ OR:       %  irun -uvmhome $UVM_HOME ex5-1_apb_transfer.sv
 
-****************************************************************/
+ ****************************************************************/
 module test;
-import uvm_pkg::*;
+  import uvm_pkg::*;
 `include "uvm_macros.svh"
-//------------------------------------------------------------------------------
-// CLASS: apb_transfer
-//------------------------------------------------------------------------------
-typedef enum bit {APB_READ, APB_WRITE} apb_direction_enum;
+  //------------------------------------------------------------------------------
+  // CLASS: apb_transfer
+  //------------------------------------------------------------------------------
+  typedef enum bit {APB_READ, APB_WRITE} apb_direction_enum;
 
-class apb_transfer extends uvm_sequence_item;                                  
+  class apb_transfer extends uvm_sequence_item;
 
-  rand bit [31:0]           addr;
-  rand bit [31:0]           data;
-  rand apb_direction_enum   direction;
-  // Control field - does not translate into signal data
-  rand int unsigned         transmit_delay;
- 
-  constraint c_default_delay { transmit_delay inside {[0:100]} ; } 
-  constraint c_default_addr { addr[1:0] == 2'b00; }
+    rand bit [31:0]           addr;
+    rand bit [31:0]           data;
+    rand apb_direction_enum   direction;
+    // Control field - does not translate into signal data
+    rand int unsigned         transmit_delay;
 
-  `uvm_object_utils_begin(apb_transfer)
-    `uvm_field_int(addr, UVM_DEFAULT)
-    `uvm_field_int(data, UVM_DEFAULT)
-    `uvm_field_enum(apb_direction_enum, direction, UVM_DEFAULT)
-    `uvm_field_int(transmit_delay, UVM_DEFAULT|UVM_NOCOMPARE|UVM_NOPACK)
-  `uvm_object_utils_end
+    constraint c_default_delay {transmit_delay inside {[0:100]} ;}
+    constraint c_default_addr {addr[1:0] == 2'b00;}
 
-  // Constructor - UVM sequence items require a string argument
-  function new (string name = "apb_transfer");
-    super.new(name);
-  endfunction
+    `uvm_object_utils_begin(apb_transfer)
+      `uvm_field_int(addr, UVM_DEFAULT)
+      `uvm_field_int(data, UVM_DEFAULT)
+      `uvm_field_enum(apb_direction_enum, direction, UVM_DEFAULT)
+      `uvm_field_int(transmit_delay, UVM_DEFAULT|UVM_NOCOMPARE|UVM_NOPACK)
+    `uvm_object_utils_end
 
-endclass : apb_transfer
+    // Constructor - UVM sequence items require a string argument
+    function new (string name = "apb_transfer");
+      super.new(name);
+    endfunction : new
+  endclass : apb_transfer
 
 
-apb_transfer my_transfer;
+  apb_transfer my_transfer;
 
-initial begin
-  my_transfer = new();
-  repeat (3) begin
-     void'(my_transfer.randomize());
-     my_transfer.print();
+  initial begin
+    my_transfer = new();
+    repeat (3) begin
+      void'(my_transfer.randomize());
+      my_transfer.print();
+    end
+    $display("UVM DEFAULT TREE PRINTER FORMAT:");
+    my_transfer.print(uvm_default_tree_printer);
+    $display("UVM DEFAULT LINE PRINTER FORMAT:");
+    my_transfer.print(uvm_default_line_printer);
   end
-  $display("UVM DEFAULT TREE PRINTER FORMAT:");
-  my_transfer.print(uvm_default_tree_printer);
-  $display("UVM DEFAULT LINE PRINTER FORMAT:");
-  my_transfer.print(uvm_default_line_printer);
-end
-
 endmodule : test
+
+
+// OUTPUT:
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'h5ff9fee8
+// #   data            integral            32    'ha7764686
+// #   direction       apb_direction_enum  1     APB_WRITE
+// #   transmit_delay  integral            32    'h42
+// # ------------------------------------------------------
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'hfcac1a4c
+// #   data            integral            32    'hee27cf6f
+// #   direction       apb_direction_enum  1     APB_WRITE
+// #   transmit_delay  integral            32    'h26
+// # ------------------------------------------------------
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'hd2a35224
+// #   data            integral            32    'hc2e81fe
+// #   direction       apb_direction_enum  1     APB_READ
+// #   transmit_delay  integral            32    'h44
+// # ------------------------------------------------------
+// # UVM DEFAULT TREE PRINTER FORMAT:
+// # apb_transfer: (apb_transfer@355) {
+// #   addr: 'hd2a35224
+// #   data: 'hc2e81fe
+// #   direction: APB_READ
+// #   transmit_delay: 'h44
+// # }
+// # UVM DEFAULT LINE PRINTER FORMAT:
+// # apb_transfer: (apb_transfer@355) { addr: 'hd2a35224  data: 'hc2e81fe  direction: APB_READ  transmit_delay: 'h44  }
--- uvm_book/examples_lib/05_interface_uvcs/ex5-2_apb_transfer1.sv
+++ uvm_book/examples_lib/05_interface_uvcs/ex5-2_apb_transfer1.sv
@@ -1,71 +1,111 @@
 
 /****************************************************************
-  Example 5-2: Defining a Transaction Control Field
+ Example 5-2: Defining a Transaction Control Field
 
-  The APB Transfer Class with an additional control knob
+ The APB Transfer Class with an additional control knob
 
-  To run:   %  irun -uvm ex5-2_apb_transfer1.sv
+ To run:   %  irun -uvm ex5-2_apb_transfer1.sv
 
-  OR:       %  irun -uvmhome $UVM_HOME ex5-2_apb_transfer1.sv
-****************************************************************/
+ OR:       %  irun -uvmhome $UVM_HOME ex5-2_apb_transfer1.sv
+ ****************************************************************/
 module test();
-import uvm_pkg::*;
+  import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-//------------------------------------------------------------------------------
-// CLASS: apb_transfer
-//------------------------------------------------------------------------------
-typedef enum bit {APB_READ, APB_WRITE} apb_direction_enum;
-typedef enum {ZERO, SHORT, MEDIUM, LONG, MAX} apb_dly_enum;
+  //------------------------------------------------------------------------------
+  // CLASS: apb_transfer
+  //------------------------------------------------------------------------------
+  typedef enum bit {APB_READ, APB_WRITE} apb_direction_enum;
+  typedef enum {ZERO, SHORT, MEDIUM, LONG, MAX} apb_dly_enum;
 
-class apb_transfer extends uvm_sequence_item;                                  
+  class apb_transfer extends uvm_sequence_item;
 
-  rand bit [31:0]           addr;
-  rand bit [31:0]           data;
-  rand apb_direction_enum   direction;
+    rand bit [31:0]           addr;
+    rand bit [31:0]           data;
+    rand apb_direction_enum   direction;
 
-  // Control fields - do not translate to signal data
-  rand apb_dly_enum         delay_kind;
-  rand int unsigned         transmit_delay;
- 
-  constraint c_addr { addr[1:0] == 2'b00; }
-  constraint c_transmit_delay { solve delay_kind before transmit_delay;
-           transmit_delay >= 0; transmit_delay <= 100 ; 
-           (delay_kind == ZERO) -> transmit_delay == 0;
-           (delay_kind == SHORT) -> transmit_delay inside {[1:10]}; 
-           (delay_kind == MEDIUM) -> transmit_delay inside {[11:29]}; 
-           (delay_kind == LONG) -> transmit_delay inside {[30:100]}; 
-           (delay_kind == MAX) -> transmit_delay == 100;  }
+    // Control fields - do not translate to signal data
+    rand apb_dly_enum         delay_kind;
+    rand int unsigned         transmit_delay;
 
-  // UVM utilities and automation macros for data items
-  `uvm_object_utils_begin(apb_transfer)
-    `uvm_field_int(addr, UVM_DEFAULT)
-    `uvm_field_int(data, UVM_DEFAULT)
-    `uvm_field_enum(apb_direction_enum, direction, UVM_DEFAULT)
-    `uvm_field_enum(apb_dly_enum, delay_kind, UVM_DEFAULT | UVM_NOCOMPARE | UVM_NOPACK)
-    `uvm_field_int(transmit_delay, UVM_DEFAULT | UVM_NOCOMPARE | UVM_NOPACK)
-  `uvm_object_utils_end
+    constraint c_addr {addr[1:0] == 2'b00;}
+    constraint c_transmit_delay {solve delay_kind before transmit_delay;
+      transmit_delay >= 0; transmit_delay <= 100 ;
+      (delay_kind == ZERO) -> transmit_delay == 0;
+      (delay_kind == SHORT) -> transmit_delay inside {[1:10]};
+      (delay_kind == MEDIUM) -> transmit_delay inside {[11:29]};
+      (delay_kind == LONG) -> transmit_delay inside {[30:100]};
+      (delay_kind == MAX) -> transmit_delay == 100;}
 
-  // Constructor - required UVM syntax
-  function new (string name = "apb_transfer");
-    super.new(name);
-  endfunction
+    // UVM utilities and automation macros for data items
+    `uvm_object_utils_begin(apb_transfer)
+      `uvm_field_int(addr, UVM_DEFAULT)
+      `uvm_field_int(data, UVM_DEFAULT)
+      `uvm_field_enum(apb_direction_enum, direction, UVM_DEFAULT)
+      `uvm_field_enum(apb_dly_enum, delay_kind, UVM_DEFAULT | UVM_NOCOMPARE | UVM_NOPACK)
+      `uvm_field_int(transmit_delay, UVM_DEFAULT | UVM_NOCOMPARE | UVM_NOPACK)
+    `uvm_object_utils_end
 
-endclass : apb_transfer
+    // Constructor - required UVM syntax
+    function new (string name = "apb_transfer");
+      super.new(name);
+    endfunction : new
 
-apb_transfer my_transfer;
+  endclass : apb_transfer
 
-initial begin
-  my_transfer = new();
-  repeat (3) begin
-     void'(my_transfer.randomize());
-     my_transfer.print();
+  apb_transfer my_transfer;
+
+  initial begin
+    my_transfer = new();
+    repeat (3) begin
+      void'(my_transfer.randomize());
+      my_transfer.print();
+    end
+    $display("my_transfer.randomize() with {delay_kind == MAX;} : ");
+    void'(my_transfer.randomize() with {delay_kind == MAX;});
+    my_transfer.print();
   end
-  $display("my_transfer.randomize() with {delay_kind == MAX;} : ");
-  void'(my_transfer.randomize() with {delay_kind == MAX;});
-  my_transfer.print();
-end
-
-
-
 endmodule : test
+
+// OUTPUT:
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'h5ff9fee8
+// #   data            integral            32    'ha7764686
+// #   direction       apb_direction_enum  1     APB_WRITE
+// #   delay_kind      apb_dly_enum        32    LONG
+// #   transmit_delay  integral            32    'h42
+// # ------------------------------------------------------
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'hfcac1a4c
+// #   data            integral            32    'hee27cf6f
+// #   direction       apb_direction_enum  1     APB_WRITE
+// #   delay_kind      apb_dly_enum        32    SHORT
+// #   transmit_delay  integral            32    'h3
+// # ------------------------------------------------------
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'hd2a35224
+// #   data            integral            32    'hc2e81fe
+// #   direction       apb_direction_enum  1     APB_READ
+// #   delay_kind      apb_dly_enum        32    ZERO
+// #   transmit_delay  integral            32    'h0
+// # ------------------------------------------------------
+// # my_transfer.randomize() with {delay_kind == MAX;} :
+// # ------------------------------------------------------
+// # Name              Type                Size  Value
+// # ------------------------------------------------------
+// # apb_transfer      apb_transfer        -     @355
+// #   addr            integral            32    'hf6bb2ce4
+// #   data            integral            32    'hbc4cc2ee
+// #   direction       apb_direction_enum  1     APB_WRITE
+// #   delay_kind      apb_dly_enum        32    MAX
+// #   transmit_delay  integral            32    'h64
+// # ------------------------------------------------------
